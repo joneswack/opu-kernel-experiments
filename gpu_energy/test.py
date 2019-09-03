d7ab2f2a073a213c2873f62bb49d7488d91bd171
Sébastien Marmin <33869096+SebastienMarmin@users.noreply.github.com>
2019-09-03 11:47:33 +0000

    energy measurement test for ridge classif

---
+++ gpu_energy/test.py
@@ -0,0 +1,29 @@
+from power_info import EnergyMonitor
+import time
+e = EnergyMonitor(period = 3)
+
+ener1 = e.energy()
+time.sleep(5)
+ener2 = e.energy()
+print(ener1)
+print(ener2)
+
+print(repr(ener1))
+print(repr(ener2))
+
+print(repr(ener2-ener1))
+print((ener2-ener1))
+print((ener2-ener1).item("2"))
+
+
+
+import matplotlib
+matplotlib.rcParams['timezone'] = 'Europe/Paris'
+import matplotlib.pyplot as plt
+
+t,p,gpu = e.to_numpy()
+
+for i in range(len(gpu)):
+    plt.plot(t[i,:],p[i,:],label="GPU "+gpu[i])
+plt.legend()
+plt.savefig("figure.png")
