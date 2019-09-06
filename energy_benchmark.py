55542a03ac4e196cf93c066b880e175c8a2977b2
Sébastien Marmin <33869096+SebastienMarmin@users.noreply.github.com>
2019-09-06 14:48:41 +0200

    first consumption gpu results

--- energy_benchmark.py
+++ energy_benchmark.py
@@ -11,7 +11,7 @@ dtype = torch.FloatTensor
 period = 2 # How often should we read (instant) power usage in seconds.
 e = EnergyMonitor(period=period)
 
-repetitions = 300 # repeat the hole experiment and take the mean
+repetitions = 600 # repeat the hole experiment and take the mean
 
 
 results_dir = "gpu_energy/results/"
@@ -26,7 +26,7 @@ print("Experiment on GPU "+str(gpu_device)+".")
 if e._ngpus != ngpus_torch:
     print("Pytorch doesn't see all GPUs. The GPUs number in EnergyMonitor corresponds to what nvidia-smi gives.")
 
-n = 320 # Number of points to be projected
+n = 100 # Number of points to be projected
 d_list = [100,320,1000,3200,10000,32000] # Their dimension
 p_list = [100,320,1000,3200,10000,32000] # Their targeted dimension
 
@@ -37,49 +37,72 @@ r = .5 # proportion of ones in the data
 t_gpu = empty((len(d_list),len(p_list)))
 E_gpu = empty((len(d_list),len(p_list)))
 P_gpu = empty((len(d_list),len(p_list)))
-
 t_cpu = empty((len(d_list),len(p_list)))
-
+t_lgpu = empty((len(d_list),len(p_list)))
+E_lgpu = empty((len(d_list),len(p_list)))
+P_lgpu = empty((len(d_list),len(p_list)))
+t_lcpu = empty((len(d_list),len(p_list)))
 cpu_limit = 5
 
 for i in range(len(d_list)):
     d = d_list[i]
     print("d = "+str(d),end=".\n")
     print("Sampling x...", end = " ")
-    x = torch.distributions.Binomial(1,r).sample((n,d)).type(dtype)
+    x = torch.distributions.Binomial(1,r).sample((repetitions,n,d)).type(dtype).to("cpu")
     print("Done.")
     for j in range(len(p_list)):
-        print("Adding x")
-        x = x+0.0*(100*i+10000*j)
-        print("Done")
         p = p_list[j]
+        print("p = "+str(p),end=".\n")
         print("Building OPU object...")
+        e1_lcpu = e.energy()
         obj = OPUModulePyTorch( d, p, initial_log_scale='auto', tunable_kernel=False, dtype=dtype).to("cpu")
-        print("Done.", end = " ")
-        x = x.to("cpu")
+        e2_lcpu = e.energy()
+        t_lcpu[i,j] = (e2_lcpu-e1_lcpu).duration()
+        print("Done.")
+
+
         if i+j<cpu_limit:
-            e1 = e.energy()
+            print("Start matmuls with cpu...")
+            e1_cpu = e.energy()
             for repe in range(repetitions):
-                _ = obj(x.detach())
-            e2 = e.energy()
-            t_cpu[i,j] = (e2-e1).duration()
+                _ = obj(x[repe,:,:].detach())
+            e2_cpu = e.energy()
+            print("Done.")
+            t_cpu[i,j] = (e2_cpu-e1_cpu).duration()
         else:
             t_cpu[i,j] = Inf
-        x = x.to(gpu_device)
+
+        print("Transfert to GPU...")
+        e1_lgpu = e.energy()
         obj.to(gpu_device)
-        e3 = e.energy()
+        e2_lgpu = e.energy()
+        print("Done.")
+        print("Start matmuls...")
+        e1_gpu = e.energy()
         for repe in range(repetitions):
-            _ = obj(x.detach())
-        e4 = e.energy()
-        e_gpu = (e4-e3).select_gpu(str(DEVICE)) # nvdia-smi number
+            _ = obj(x[repe,:,:].to(gpu_device).detach())
+        e2_gpu = e.energy()
+        print("Done.")
+        e_lgpu = (e2_lgpu-e1_lgpu).select_gpu(str(DEVICE)) # nvdia-smi number
+        t_lgpu[i,j] = e_lgpu.duration() + t_lcpu[i,j]    
+        E_lgpu[i,j] = e_lgpu.consumption() 
+        P_lgpu[i,j] = E_lgpu[i,j]/t_lgpu[i,j]
+
+        e_gpu  = (e2_gpu-e1_gpu).select_gpu(str(DEVICE)) 
         t_gpu[i,j] = e_gpu.duration()/repetitions    # take the mean
         E_gpu[i,j] = e_gpu.consumption()/repetitions
         P_gpu[i,j] = E_gpu[i,j]/t_gpu[i,j]
-        if j< len(p_list)-1 :
-            print("p = "+str(p_list[j]),end=", ")
-        else:
-            print("p = "+str(p_list[j]),end=".\n")
 
+
+
+print("CPU loading time:")
+print(t_lcpu)
+print("GPU loading time:")
+print(t_lgpu)
+print("GPU loading energy (J):")
+print(E_lgpu)
+print("GPU average loading power (W):")
+print(P_lgpu)
 
 print("CPU time:")
 print(t_cpu)
@@ -108,3 +131,7 @@ plt.savefig(results_dir+"consumption.png")
 np.save(results_dir+"cpu_time",t_cpu) 
 np.save(results_dir+"gpu_time",t_gpu) 
 np.save(results_dir+"gpu_energy",E_gpu) 
+
+np.save(results_dir+"cpu_loadingtime",t_lcpu) 
+np.save(results_dir+"gpu_loadingtime",t_lgpu) 
+np.save(results_dir+"gpu_loadingenergy",E_lgpu) 
