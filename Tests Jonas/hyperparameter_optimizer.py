3bb2f2a790246e0babf5e5fef896866465021b23
Jonas Wacker <jonas.wacker@gmail.com>
2019-11-04 23:33:12 +0100

    implemented hyperparameter search

--- Tests Jonas/hyperparameter_optimizer.py
+++ Tests Jonas/hyperparameter_optimizer.py
@@ -7,9 +7,11 @@ import collections
 
 import numpy as np
 import torch
+from sklearn.model_selection import train_test_split
 
 import util.data
-import util.cg_torch
+from util.ridge_regression import RidgeRegression
+from util.random_features import project_np_data
 
 # We avoid using CUDA_VISIBLE_DEVICES here and assume that this variable is set from the command line!
 # os.environ["CUDA_VISIBLE_DEVICES"]="2,3"
@@ -56,8 +58,20 @@ def hyperparameter_iterator(cv_hyperparameters):
         for combo in itertools.product(*list(kernel_params.values())):
             yield {'alpha': alpha, 'kernel_params': dict(zip(kernel_params.keys(), combo))}
 
-def run_experiment(train_data, test_data, train_labels, test_labels, hyperparams):
-    return True
+def run_experiment(train_data, test_data, train_labels, test_labels, proj_params, alpha):
+    projection, _ = project_np_data(np.vstack([train_data, test_data]), **proj_params)
+
+    # compute train_test split on training data to create validation set
+    X_train, X_val, y_train, y_val = train_test_split(
+        projection[:len(train_data)], train_labels, test_size=0.2, random_state=42)
+
+    clf = RidgeRegression(solver='cholesky_pytorch', kernel=None)
+    clf.fit(X_train, y_train, alpha)
+
+    val_score = clf.score(X_val, y_val)
+    test_score = clf.score(projection[len(train_data):], test_labels)
+    
+    return val_score, test_score
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -84,16 +98,21 @@ if __name__ == '__main__':
     # iterate over all the kernel configs
     for kernel, params in hyperparameter_config.items():
         print('Running experiments for kernel {}'.format(kernel))
-        converted_hyperparameters = convert_cv_hyperparameters_dict(params['cv_hyperparameters'])
-        other_parameters = {k:v for k,v in params.items() if k != 'cv_hyperparameters'}
+        converted_cv_hyperparameters = convert_cv_hyperparameters_dict(params['cv_hyperparameters'])
+        other_hyperparams = {k:v for k,v in params.items() if k != 'cv_hyperparameters'}
 
         # iterate over all cv hyperparameters
-        for hyperparams in hyperparameter_iterator(converted_hyperparameters):
-            print('Current configuration: {}'.format(hyperparams))
-            run_experiment(train_data, test_data, train_labels, test_labels, hyperparams)
+        for cv_hyperparams in hyperparameter_iterator(converted_cv_hyperparameters):
+            print('Current configuration: {}'.format(cv_hyperparams))
 
-    # since = time.time()
-    # dual_coef, iterations = util.cg_torch.MultiCGGPU(K, train_labels, tol=1e-11, atol=1e-9, max_iterations=10*N, cuda=True, num_gpus=2)
+            proj_params = {**cv_hyperparams['kernel_params'], **other_hyperparams}
+            alpha = cv_hyperparams['alpha']
 
-    # print('Done. Iterations:', iterations)
-    # print('Time:', time.time() - since)
+            since = time.time()
+            val_score, test_score = run_experiment(
+                train_data, test_data, train_labels, test_labels, proj_params, alpha)
+            elapsed = time.time() - since
+
+            print('Validation Score: {}'.format(val_score))
+            print('Test Score: {}'.format(test_score))
+            print('Time taken: {}'.format(elapsed))
